603fafc325fa9ee2e895f8a9ca95995572ba6b30
Sauban-Git <128286356+Sauban-Git@users.noreply.github.com>
2024-09-17 15:12:09 +0530

    Update build.gradle.kts version updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.sauban.mymemory"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
