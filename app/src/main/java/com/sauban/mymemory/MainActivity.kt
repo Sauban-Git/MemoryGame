bab7702bbf954f7c8b3be371aa662d994aa7883e
Sauban-Git <128286356+Sauban-Git@users.noreply.github.com>
2024-10-01 01:33:57 +0530

    Update MainActivity.kt

--- app/src/main/java/com/sauban/mymemory/MainActivity.kt
+++ app/src/main/java/com/sauban/mymemory/MainActivity.kt
@@ -307,7 +307,7 @@ class MainActivity : AppCompatActivity() {
 
 //        For extra moves or game over
         if (memoryGame.getNumMoves() >= checkMoves && !extraMoveUsed) {
-            Toast.makeText(this, "Game over! Try again.",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Out of Move!",Toast.LENGTH_SHORT).show()
             AlertDialog.Builder(this)
                 .setTitle("Out of Moves!")
                 .setMessage("You're out of Moves! Would you like to try again with extra moves?")
