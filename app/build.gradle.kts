cd82c1014fc1536315d281872deb23ee8127bd88
Sauban-Git <128286356+Sauban-Git@users.noreply.github.com>
2024-10-01 01:50:32 +0530

    Testind shrink Resource

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
         release {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
